8c8d55b930a7bae3b9746762f6de9adfd160f647
Matthieu Bonbon <matthieu.bonbon@etu.univ-orleans.fr>
2023-05-02 21:15:57 +0200

    colors added

---
+++ tp_opengl/app/src/main/java/com/example/tp_opengl/constants/Colors.java
@@ -0,0 +1,63 @@
+package com.example.tp_opengl.constants;
+
+public class Colors {
+
+    // Tetris colors
+
+    public static final float[] cyan = { // CYAN
+            0.0f,  1.0f, 1.0f, 1.0f,
+            0.0f,  1.0f, 1.0f, 1.0f,
+            0.0f,  1.0f, 1.0f, 1.0f,
+            0.0f,  1.0f, 1.0f, 1.0f
+    };
+
+    public static final float[] orange = { // ORANGE
+            1.0f,  0.67f, 0.0f, 1.0f,
+            1.0f,  0.67f, 0.0f, 1.0f,
+            1.0f,  0.67f, 0.0f, 1.0f,
+            1.0f,  0.67f, 0.0f, 1.0f
+    };
+
+
+    public static final float[] yellow = { // YELLOW
+            1.0f,  1.0f, 0.0f, 1.0f,
+            1.0f,  1.0f, 0.0f, 1.0f,
+            1.0f,  1.0f, 0.0f, 1.0f,
+            1.0f,  1.0f, 0.0f, 1.0f
+    };
+
+    public static final float[] purple = { // PURPLE
+            0.6f,  0.0f, 1.0f, 1.0f,
+            0.6f,  0.0f, 1.0f, 1.0f,
+            0.6f,  0.0f, 1.0f, 1.0f,
+            0.6f,  0.0f, 1.0f, 1.0f
+    };
+
+    public static final float[] red = { // RED
+            1.0f,  0.0f, 0.0f, 1.0f,
+            1.0f,  0.0f, 0.0f, 1.0f,
+            1.0f,  0.0f, 0.0f, 1.0f,
+            1.0f,  0.0f, 0.0f, 1.0f
+    };
+
+    public static final float[] green = { // GREEN
+            0.0f,  1.0f, 0.0f, 1.0f,
+            0.0f,  1.0f, 0.0f, 1.0f,
+            0.0f,  1.0f, 0.0f, 1.0f,
+            0.0f,  1.0f, 0.0f, 1.0f
+    };
+
+    public static final float[] blue = { // BLUE
+            0.0f,  0.0f, 1.0f, 1.0f,
+            0.0f,  0.0f, 1.0f, 1.0f,
+            0.0f,  0.0f, 1.0f, 1.0f,
+            0.0f,  0.0f, 1.0f, 1.0f
+    };
+
+    public static final float[] white = { // WHITE
+            1.0f,  1.0f, 1.0f, 1.0f,
+            1.0f,  1.0f, 1.0f, 1.0f,
+            1.0f,  1.0f, 1.0f, 1.0f,
+            1.0f,  1.0f, 1.0f, 1.0f
+    };
+}
