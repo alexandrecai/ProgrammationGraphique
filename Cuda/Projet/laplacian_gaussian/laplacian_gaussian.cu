4a11c60a55e2f619ebc28fb53ce37157e1d67c70
Cailloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2023-05-09 23:48:43 +0200

    push

--- Cuda/Projet/laplacian_gaussian/laplacian_gaussian.cu
+++ Cuda/Projet/laplacian_gaussian/laplacian_gaussian.cu
@@ -81,7 +81,7 @@ __global__ void laplacian_gaussian_shared( unsigned char * g, unsigned char * s,
 
   __syncthreads();
 
-  if( i < cols -1 && j < rows-1 && li > 0 && li < (w-1) && lj > 0 && lj < (h-1) )
+  if( i < cols -2 && j < rows-2 && li > 1 && li < (w-2) && lj > 1 && lj < (h-2) )
   {
       /*
     auto h =     sh[ (lj-1)*w + li - 1 ] -     sh[ (lj-1)*w + li + 1 ]
