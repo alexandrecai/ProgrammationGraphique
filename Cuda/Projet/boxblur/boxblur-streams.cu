160dc78568c5db6bc3319e1517485c9abb4c04fc
Cailloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2023-05-11 22:26:06 +0200

    push

--- Cuda/Projet/boxblur/boxblur-streams.cu
+++ Cuda/Projet/boxblur/boxblur-streams.cu
@@ -1,74 +1,89 @@
-#include <iostream>
+#include <cuda_runtime.h>
 #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+#include <chrono>
 
-__global__ void boxBlur(const cv::cuda::PtrStep<uchar3> src,
-                        cv::cuda::PtrStep<uchar3> dst,
-                        const int kernel_size) {
-    const int x = blockDim.x * blockIdx.x + threadIdx.x;
-    const int y = blockDim.y * blockIdx.y + threadIdx.y;
-    if (x < src.cols && y < src.rows) {
-        float3 sum = make_float3(0.f, 0.f, 0.f);
-        int count = 0;
-        for (int dy = -kernel_size; dy <= kernel_size; ++dy) {
-            for (int dx = -kernel_size; dx <= kernel_size; ++dx) {
-                const int nx = x + dx;
-                const int ny = y + dy;
-                if (nx >= 0 && nx < src.cols && ny >= 0 && ny < src.rows) {
-                    const uchar3 p = src(ny, nx);
-                    sum += make_float3(p.x, p.y, p.z);
-                    count += 1;
-                }
-            }
-        }
-        const float scale = 1.f / static_cast<float>(count);
-        const uchar3 avg = make_uchar3(sum.x * scale, sum.y * scale, sum.z * scale);
-        dst(y, x) = avg;
+__global__ void grayscale_boxblur_shared(unsigned char* rgb, unsigned char* s, std::size_t cols, std::size_t rows)
+{
+    auto i = blockIdx.x * (blockDim.x - 2) + threadIdx.x;
+    auto j = blockIdx.y * (blockDim.y - 2) + threadIdx.y;
+
+    auto li = threadIdx.x;
+    auto lj = threadIdx.y;
+
+    auto w = blockDim.x;
+    auto h = blockDim.y;
+
+    extern __shared__ unsigned char sh[];
+
+    if (i < cols && j < rows) {
+        sh[lj * w + li] = (307 * rgb[3 * (j * cols + i)] + 604 * rgb[3 * (j * cols + i) + 1] + 113 * rgb[3 * (j * cols + i) + 2]) >> 10;
+    }
+
+    __syncthreads();
+
+    if (i < cols - 1 && j < rows - 1 && li > 0 && li < (w - 1) && lj > 0 && lj < (h - 1))
+    {
+        auto total = sh[((lj - 1) * w + li - 1)] + sh[((lj - 1) * w + li)] + sh[((lj - 1) * w + li + 1)]
+                     + sh[(lj * w + li - 1)] + sh[(lj * w + li)] + sh[(lj * w + li + 1)]
+                     + sh[((lj + 1) * w + li - 1)] + sh[((lj + 1) * w + li)] + sh[((lj + 1) * w + li + 1)];
+
+        auto res = total / 9;
+        s[j * cols + i] = res;
     }
 }
 
 int main() {
-    cv::Mat img = cv::imread("../images/input.jpg", cv::IMREAD_COLOR);
-    cv::cuda::GpuMat d_src(img);
-    cv::cuda::GpuMat d_dst(d_src.size(), d_src.type());
+    // Lecture de l'image d'entrée
+    cv::Mat m_in = cv::imread("../images/in.jpg", cv::IMREAD_UNCHANGED);
+    auto rows = m_in.rows;
+    auto cols = m_in.cols;
 
-    const dim3 block_size(32, 32);
-    const dim3 grid_size((d_src.cols - 1) / block_size.x + 1,
-                         (d_src.rows - 1) / block_size.y + 1);
-    const int kernel_size = 3;
+    // Allocation et copie des données sur le GPU
+    unsigned char* rgb_d = nullptr;
+    unsigned char* g_d = nullptr;
+    unsigned char* s_d = nullptr;
+    cudaMalloc(&rgb_d, 3 * rows * cols);
+    cudaMalloc(&g_d, rows * cols);
+    cudaMalloc(&s_d, rows * cols);
+    cudaMemcpy(rgb_d, m_in.data, 3 * rows * cols, cudaMemcpyHostToDevice);
 
-    // Streams declaration.
-    cudaStream_t streams[2];
+    // Définition des paramètres de grille et de bloc pour les kernels
+    dim3 block(32, 4);
+    dim3 grid0((cols - 1) / block.x + 1, (rows - 1) / block.y + 1);
+    dim3 grid1((cols - 1) / (block.x - 2) + 1, (rows - 1) / (block.y - 2) + 1);
 
-    // Creation.
-    cudaStreamCreate(&streams[0]);
-    cudaStreamCreate(&streams[1]);
+    // Création des streams CUDA
+    cudaStream_t stream[2];
+    cudaStreamCreate(&stream[0]);
+    cudaStreamCreate(&stream[1]);
 
-    // Copying input image to device by halves.
-    cudaMemcpyAsync(d_src.ptr(), img.data, d_src.cols * d_src.rows * sizeof(uchar3) / 2,
-                    cudaMemcpyHostToDevice, streams[0]);
-    cudaMemcpyAsync(d_src.ptr(d_src.rows / 2), img.data + d_src.cols * d_src.rows * sizeof(uchar3) / 2,
-                    d_src.cols * d_src.rows * sizeof(uchar3) / 2, cudaMemcpyHostToDevice, streams[1]);
+    // Appel du premier kernel
+    grayscale_boxblur_shared<<<grid1, block, block.x * (block.y + 2) * sizeof(unsigned char), stream[0]>>>(rgb_d, g_d, s_d, cols, rows);
 
-    // Launching one kernel in each stream.
-    boxBlur<<<grid_size, block_size, 0, streams[0]>>>(d_src, d_dst, kernel_size);
-    boxBlur<<<grid_size, block_size, 0, streams[1]>>>(d_src.ptr(d_src.rows / 2), d_dst.ptr(d_src.rows / 2), kernel_size);
+    // Appel du deuxième kernel
+    grayscale_boxblur_shared<<<grid0, block, block.x * block.y * sizeof(unsigned char), stream[1]>>>(g_d, g_d, s_d, cols, rows);
 
-    // Copying output image from device by halves.
-    cudaMemcpyAsync(img.data, d_dst.ptr(), d_src.cols * d_src.rows * sizeof(uchar3) / 2,
-                    cudaMemcpyDeviceToHost, streams[0]);
-    cudaMemcpyAsync(img.data + d_src.cols * d_src.rows * sizeof(uchar3) / 2, d_dst.ptr(d_src.rows / 2),
-                    d_src.cols * d_src.rows *
-                            sizeof(uchar3) / 2, cudaMemcpyDeviceToHost, streams[1]);
+    // Copie du résultat final sur le CPU
+    unsigned char* out = nullptr;
+    cudaMallocHost(&out, rows * cols);
+    cudaMemcpyAsync(out, s_d, rows * cols, cudaMemcpyDeviceToHost, stream[1]);
 
-// Synchronizing streams.
-    cudaStreamSynchronize(streams[0]);
-    cudaStreamSynchronize(streams[1]);
+    // Affichage du temps d'exécution
+    cudaDeviceSynchronize();
+    auto start = std::chrono::high_resolution_clock::now();
+    cudaDeviceSynchronize();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
 
-// Destroying streams.
-    cudaStreamDestroy(streams[0]);
-    cudaStreamDestroy(streams[1]);
-
-    cv::imwrite("out.jpg", img);
+    // Libération de la mémoire
+    cudaFree(rgb_d);
+    cudaFree(g_d);
+    cudaFree(s_d);
+    cudaFreeHost(out);
+    cudaStreamDestroy(stream[0]);
+    cudaStreamDestroy(stream[1]);
 
     return 0;
 }
