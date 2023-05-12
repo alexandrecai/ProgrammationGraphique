53063432d4b1f96ccc9fb442a65a2462e0477311
Cailloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2023-05-12 17:07:21 +0200

    push

--- Cuda/Projet/boxblur/boxblur-streams.cu
+++ Cuda/Projet/boxblur/boxblur-streams.cu
@@ -65,7 +65,7 @@ int main() {
     grayscale_boxblur_shared<<<grid1, block, block.x * (block.y+2) * sizeof(unsigned char), stream[0]>>>(rgb_d, s_d, cols, rows/2+1);
 
     // Appel du deuxième kernel
-    grayscale_boxblur_shared<<<grid1, block, block.x * (block.y+2) * sizeof(unsigned char), stream[1]>>>(rgb_d+(((rows-1)*cols*3)/2), g_d, cols, rows/2+1);
+    grayscale_boxblur_shared<<<grid1, block, block.x * (block.y+2) * sizeof(unsigned char), stream[1]>>>(rgb_d+(((rows*cols*3)/2)-cols*3), g_d, cols, rows/2+1);
 
     // Copie du résultat final sur le CPU
     unsigned char* out = nullptr;
