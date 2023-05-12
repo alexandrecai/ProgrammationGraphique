33524ec0e849168dabd5b3d7b77da80d9e017f63
Cailloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2023-05-12 14:57:14 +0200

    push

--- Cuda/Projet/boxblur/boxblur-streams.cu
+++ Cuda/Projet/boxblur/boxblur-streams.cu
@@ -71,7 +71,7 @@ int main() {
     unsigned char* out = nullptr;
     cudaMallocHost(&out, rows * cols);
     //cudaMemcpyAsync(out, s_d, (rows * cols)/2, cudaMemcpyDeviceToHost, stream[0]);
-    cudaMemcpyAsync(out, g_d, rows * cols, cudaMemcpyDeviceToHost, stream[0]);
+    cudaMemcpyAsync(out, s_d, rows * cols, cudaMemcpyDeviceToHost, stream[0]);
     //cudaMemcpyAsync(out+(rows * cols)/2, g_d, (rows * cols)/2, cudaMemcpyDeviceToHost, stream[1]);
     //cudaMemcpyAsync(out+(rows * cols)/2, s_d+(rows * cols)/2, (rows * cols)/2, cudaMemcpyDeviceToHost, stream[1]);
 
